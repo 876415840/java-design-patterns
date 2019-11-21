675eb8b40f4e375a184b41264ed3d3c539eca423
mengqinghao <mengqinghao@finupgroup.com>
2019-11-21 18:24:43 +0800

    适配器、桥接、装饰

---
+++ src/main/java/com/alan/adapter/domain/Adaptee.java
@@ -0,0 +1,20 @@
+package com.alan.adapter.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description: 适配者
+ * @Author MengQingHao
+ * @Date 2019/11/21 4:40 下午
+ * @Version 1.0
+ */
+public class Adaptee {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Adaptee.class);
+
+    public void specificRequest() {
+        LOGGER.info("适配者中的业务代码被调用！");
+    }
+
+}
---
+++ src/main/java/com/alan/bridge/domain/RefinedAbstraction.java
@@ -0,0 +1,25 @@
+package com.alan.bridge.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description: 扩展抽象化角色
+ * @Author MengQingHao
+ * @Date 2019/11/21 5:34 下午
+ * @Version 1.0
+ */
+public class RefinedAbstraction extends Abstraction {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RefinedAbstraction.class);
+
+    public RefinedAbstraction(Implementor implementor) {
+        super(implementor);
+    }
+
+    @Override
+    public void operation() {
+        LOGGER.info("扩展抽象化(Refined Abstraction)角色被访问" );
+        implementor.operationImpl();
+    }
+}
