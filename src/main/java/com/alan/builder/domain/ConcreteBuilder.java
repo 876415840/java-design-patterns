fba5f259434a98da43226b50cdc091d60eb85cbe
mengqinghao <mengqinghao@finupgroup.com>
2019-11-21 16:09:12 +0800

    建造者、代理

---
+++ src/main/java/com/alan/builder/domain/ConcreteBuilder.java
@@ -0,0 +1,25 @@
+package com.alan.builder.domain;
+
+/**
+ * @Description: 具体建造者：实现了抽象建造者接口。
+ * @Author MengQingHao
+ * @Date 2019/11/8 12:03 下午
+ * @Version 1.0
+ */
+public class ConcreteBuilder extends Builder {
+
+    @Override
+    public void buildPartA() {
+        product.setPartA("建造 PartA");
+    }
+
+    @Override
+    public void buildPartB() {
+        product.setPartB("建造 PartB");
+    }
+
+    @Override
+    public void buildPartC() {
+        product.setPartC("建造 PartC");
+    }
+}
